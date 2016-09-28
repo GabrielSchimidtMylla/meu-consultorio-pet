5a1753fd23d5681d5049deab9d478ad761e68fbf
Robson Moura <robson.moura88@gmail.com>
2016-09-27 23:02:12 -0300

    ajustando versao mobile e scroll animado

--- js/wellcome.js
+++ js/wellcome.js
@@ -1,20 +1,29 @@
 //<!--INICIANDO ANIMAÇÕES-->
 new WOW().init();
-
-$(function() {
-  $(window).on("scroll", function() {
-    if($(window).scrollTop() > 100) {
+$(function () {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 100) {
       $(".nav-pet").addClass("menuScroll");
-    } else {
+    }
+    else {
       $(".nav-pet").removeClass("menuScroll");
     }
   });
 });
-$(".fa-bars").click(function(){
-  if($(this).hasClass("fa-bars")){
+$(".fa-bars").click(function () {
+  if ($(this).hasClass("fa-bars")) {
     $(this).removeClass("fa-bars");
     $(this).addClass("fa-times");
-  }else{
+  }
+  else {
     $(this).addClass("fa-bars");
   }
 })
+$(function () {
+  $('a').bind('click', function (event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1000, 'linear');
+  });
+});
